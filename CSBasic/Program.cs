e160a0227eb26230aa9614e5f76a5342069f465e
Gihun Ham <ham@e-mirim.hs.kr>
2020-04-28 14:21:49 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,36 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "윤" + "인" + "성";
+            Console.Write("Hello C#");
+            int add = 2;
+            int Alpha;
+            //주석
+            /*
+             * 
+             *  여러줄 주석 
+             * 
+             */
+
+            Console.WriteLine("Hello World");
+            Console.Write("Hello World");
+            Console.Write("Hello World");
+
+            Console.Write(52);
+            Console.WriteLine(7 % 3);
+            Console.WriteLine('A');
+            Console.WriteLine("A");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+            Console.WriteLine("가" + "나" + "다");
+
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(true);
+
         }
     }
 }
